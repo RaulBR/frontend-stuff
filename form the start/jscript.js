2128969d2a021937f9e8b2131da5eccf26f7fd92
DESKTOP-J76FNP8\Raul <ranete.raul@yahoo.com>
2018-03-20 01:28:45 +0200

    Scrool

--- form the start/jscript.js
+++ form the start/jscript.js
@@ -4,30 +4,25 @@
 
 function changeTestimonial(direction) {
     let selectors = document.getElementsByClassName("picker-position");
-    let pointer = getNextPoint(selectors, direction);
-    selectors[pointer].classList.add('selected');
+    let position = getRemovedPosition(selectors);
+    let pointer =  getNextPosition(position,direction,selectors.length);
     setTestimonialText(pointer);
+    //set dot indicator
+    selectors[pointer].classList.add('selected');  
 }
 
-function setTestimonialText(pointer) {
-    let testimonials = getTestimonials();
-    document.getElementsByClassName("testimonial-text")[0].innerHTML = "<p>"+testimonials[pointer].text +"</p>";
-    document.getElementsByClassName("autor-text")[0].innerHTML = testimonials[pointer].autorName;
-    document.getElementsByClassName("autor-ocupation")[0].innerHTML = testimonials[pointer].ocupation;
-}
-
-function getNextPoint(selectors, direction) {
-  
-    let maxPosition = selectors.length - 1;
-    let position = getRemuvedPosition(selectors);
-    let nextVal= position + direction;
-    if (direction != 0){
-    return getPointer(nextVal,maxPosition);
-    } else{
-        console.log('here');
+function getNextPosition(position,direction,length){
+    let maxPosition = length - 1;
+    let nextVal = position + direction;
+    if (nextVal < 0) {
+        nextVal = maxPosition
+    } else if (nextVal > maxPosition) {
+        nextVal = 0;
     }
+    return nextVal;
 }
-function getRemuvedPosition(selectors){
+
+function getRemovedPosition(selectors){
     let position;
     Array.from(selectors).forEach((selected, index) => {
         if (selected.classList.contains('selected')) {
@@ -38,23 +33,19 @@ function getRemuvedPosition(selectors){
     return position;  
 }
 
-function getPointer(pointer, maxPosition) {
-     if (pointer < 0) {
-        pointer = maxPosition
-    } else if (pointer > maxPosition) {
-        pointer = 0;
-    }
-    return pointer;
+function setTestimonialText(pointer) {
+    let testimonials = getTestimonials();
+    document.getElementsByClassName("testimonial-text")[0].innerHTML = "<p>"+testimonials[pointer].text +"</p>";
+    document.getElementsByClassName("autor-text")[0].innerHTML = testimonials[pointer].autorName;
+    document.getElementsByClassName("autor-ocupation")[0].innerHTML = testimonials[pointer].ocupation;
 }
-
-
 
 function getTestimonials() {
     let testimoniales = [
         {
             text: `This is my text. There are many like it, but this one is mine. My text is my best friend. 
-        It is my life. I must master it as I must master my life. Without me, my text is useless. Without my text,
-         I am useless.`,
+                     It is my life. I must master it as I must master my life. Without me, my text is useless. Without my text,
+                     I am useless.`,
             autorName: 'Vasile Blaga',
             ocupation: 'Autor in general uneori si filosof'
         },
@@ -63,17 +54,18 @@ function getTestimonials() {
             autorName: 'Bitum Uscat',
             ocupation: 'DIFilozof'
         },
-        { text: 'In textul 3 se regasesc secretele unifersului, pritre ele este si cel mai important dinte ele si anume: daca spui un secret, nu mai e secret (numa zic) ', autorName: 'Vecin Udelatrei', ocupation: 'Trilosov Terodactol' }
+        {   text: `In textul 3 se regasesc secretele unifersului, pritre ele este si cel mai important dinte ele si anume
+                    daca spui un secret, nu mai e secret (numa zic)`,
+            autorName: 'Vecin Udelatrei',
+            ocupation: 'Trilosov Terodactol' }
 
     ];
     return testimoniales;
-
-
 }
 
 function goToThisPosition(x){
     let selectors = document.getElementsByClassName("picker-position");
-    let position = getRemuvedPosition(selectors);
+    let position = getRemovedPosition(selectors);
     selectors[x].classList.add('selected');
     setTestimonialText(x);
 }
@@ -81,7 +73,32 @@ function scroolSmooth() {
     document.getElementsByClassName("background-png")[0]
         .scrollIntoView({
             behavior: 'smooth'
-
         });
+}
 
+function gohere(x){
+    if (x.innerHTML === "Home"){
+        document.getElementsByClassName("background-png")[0]
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+    if (x.innerHTML === "Services"){
+        document.getElementsByClassName("service-section")[0]
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+    if (x.innerHTML === "Testimoniales"){
+        document.getElementsByClassName("testimoniales")[0]
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+    if (x.innerHTML === "Contact us"){
+        document.getElementsByClassName("contacts")[0]
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
 }
