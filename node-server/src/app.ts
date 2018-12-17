1c8e2b6cdf61aa684345497369c14216d4f0f816
Raul <ranete.raul@yahoo.com>
2018-12-17 23:11:44 +0200

    node server

---
+++ node-server/src/app.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Routes } from "./routes/routes";
+import * as mongoose from "mongoose";
+export class App {
+    public mongoUrl: string = 'mongodb://localhost/CRMdb';  
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+    constructor() {
+        this.app = express();
+        this.config();     
+        this.routePrv.routes(this.app);  
+        this.mongoSetup();   
+    }
+
+    private config(): void{
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);    
+    }
+}
+
+export default new App().app; 
